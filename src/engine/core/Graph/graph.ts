2932d63efef521566e0f09b1041d05fda4bb0ca0
Navnath Kadam <navnathkadam284@gmail.com>
2026-03-03 21:49:20 +0530

    feat: added Graph Engine Little Logic

--- src/engine/core/Graph/graph.ts
+++ src/engine/core/Graph/graph.ts
@@ -1,9 +1,41 @@
-class GraphManager {
-  private _Nodes;
-  private _Edges;
+import type { GraphNode } from "../../types";
 
-  constructor(Nodes: any[], Edges: any[]) {
-    this._Edges = Edges;
-    this._Nodes = Nodes;
+class GraphManager {
+  id: string;
+
+  // here -1 represents the root
+  Nodes: Map<GraphNode, GraphNode[]> = new Map([
+    [{ id: "-1", name: "root" }, []],
+  ]);
+
+  constructor(id: string) {
+    this.id = id;
+  }
+
+  // add into the graph
+  addIntoGraph(from: any, to: any) {
+    this.Nodes.set(from, to);
+    return 1;
+  }
+
+  // get all the neighbouts for servers
+  getAllNextNodeFromGraph(from: GraphNode): GraphNode[] {
+    if (this.Nodes.has(from)) {
+      return [{ id: "-1", name: "nothing" }];
+    }
+
+    return this.Nodes.get(from) || [];
+  }
+
+  // get first one because sometimes we only need first
+  getNextImmediateNodeFromGraph(from: GraphNode): GraphNode {
+    if (this.Nodes.has(from)) {
+      return { id: "-1", name: "nothing" };
+    }
+
+    const arr = this.Nodes.get(from);
+    return arr ? arr[0] : { id: "-1", name: "nothing" };
   }
 }
+
+export { GraphManager };
---
+++ src/engine/types.ts
@@ -0,0 +1,6 @@
+type GraphNode = {
+  id: string;
+  name: string;
+};
+
+export type { GraphNode };
